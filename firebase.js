31f30be599a889d427f05856855ed96437ba5a93
Fedex2026 <gruasmetro@gmail.com>
2026-07-12 03:58:38 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -13,21 +13,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
 
 const firebaseConfig = {
-
-    apiKey: "TU_API_KEY",
-
-    authDomain: "TU_AUTH_DOMAIN",
-
-    databaseURL: "TU_DATABASE_URL",
-
-    projectId: "TU_PROJECT_ID",
-
-    storageBucket: "TU_STORAGE_BUCKET",
-
-    messagingSenderId: "TU_MESSAGING_SENDER_ID",
-
-    appId: "TU_APP_ID"
-
+  apiKey: "AIzaSyB_TGANBa25toDvj8LXltQLq3_-YCsbQ0A",
+  authDomain: "mantenimiento--de-flotilla.firebaseapp.com",
+  databaseURL: "https://mantenimiento--de-flotilla-default-rtdb.firebaseio.com",
+  projectId: "mantenimiento--de-flotilla",
+  storageBucket: "mantenimiento--de-flotilla.firebasestorage.app",
+  messagingSenderId: "348935340353",
+  appId: "1:348935340353:web:92eb3021548ec9ec9321c0"
 };
 
 const app = initializeApp(firebaseConfig);
